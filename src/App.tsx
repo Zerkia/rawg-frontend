729cb229d679a3417cab39eeed074d7e90e126ac
Zerkia <lordzerkia@gmail.com>
2024-02-12 10:16:35 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem bg="orange.300" area={"nav"}>
+          <NavBar />
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="pink.300" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
