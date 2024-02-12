d025e37ce85fa2719d2b76f89ec57a161373e5aa
Zerkia <lordzerkia@gmail.com>
2024-02-12 10:38:21 +0100

    theme / navbar components added

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,17 @@
+import { HStack, Switch, useColorMode, Text } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack justifyContent="space-between">
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      ></Switch>
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
